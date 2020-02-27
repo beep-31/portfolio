d705d5d126f7402cae437eec885b314481b12f2b
Alex <ascheglov17@gmail.com>
2020-02-27 17:48:20 +0100

    Typed.js finally was connected

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,12 @@
-function typeAnimation() {
-    Typed.new("#writing-text", {
-        strings: [
-            "am a beginner front-end Web Developer.", "love everything about code.", "can solve your problems."
-        ],
-        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-        stringsElement: null,
-        // typing speed
-        typeSpeed: 1,
-        contentType: 'text',
-        callback: function() {
-            $("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-        },
-        preStringTyped: function() {},
-        onStringTyped: function() {}
-    });
-}
 
-typeAnimation();
+let element = document.querySelector('#writing_text');
+
+var typed = new Typed( element, {
+    strings: ['Some strings without', 'Some HTML', 'Chars'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    bindInputFocusEvents: true,
+    loop: true,
+    stringsElement: null,
+    contentType: 'text'
+});
