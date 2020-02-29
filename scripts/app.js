5f790ca3450b8347c6f30f9a090274a1dcb77d72
Alex <ascheglov17@gmail.com>
2020-02-29 17:07:08 +0100

    Github link was added and animation was completely finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,22 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let element = document.querySelector('#writing_text');
+    var typed = new Typed( element, {
+        strings: ['am a front-end web developer', 'love coding so much', 'am ready to solve your problems'],
+        typeSpeed: 30,
+        backSpeed: 40,
+        // bindInputFocusEvents: true,
+        loop: false,
+        stringsElement: null,
+        contentType: 'text',
+        onComplete: function(){
+            element.style.color = "#fff";
+            element.style.backgroundColor = "#e67e22";
+            let cursor = document.querySelector(".typed-cursor");
+            console.log(cursor);
+            cursor.classList.remove("typed-cursor--blink");
+        }
 
-let element = document.querySelector('#writing_text');
+    });
 
-var typed = new Typed( element, {
-    strings: ['am a front-end web developer', 'love coding so much', 'am ready to solve your problems'],
-    typeSpeed: 30,
-    backSpeed: 40,
-    // bindInputFocusEvents: true,
-    loop: false,
-    stringsElement: null,
-    contentType: 'text',
-    onComplete: function(){
-        element.style.color = "#fff";
-        element.style.backgroundColor = "#e67e22";
-    }
-    // preStringTyped: function() {},
-	// onStringTyped: function() {}
+
 });
